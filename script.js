847a9e3d812f77185b494cad3926061f6987afc8
Praveen S G <144553645+praveensg0@users.noreply.github.com>
2023-10-13 01:34:23 +0530

    made the code responsive

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-    if (inputBox.value === '') {
+    const inputBox = document.getElementById("input-box");
+    const taskText = inputBox.value.trim(); // Trim leading/trailing white spaces
+    if (taskText === '') {
         alert("You need to write something!");
+    } else if (taskText.length > 50) {
+        alert("Text should be within 50 characters.");
     } else {
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        li.innerHTML = taskText;
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
@@ -15,6 +19,26 @@ function addTask() {
     inputBox.value = "";
     saveData();
 }
+
+function adjustAddButtonSize() {
+    const inputTextLength = inputBox.value.length;
+    const minButtonWidth = 100; // Minimum button width
+    const maxButtonWidth = 200; // Maximum button width
+    const currentButtonWidth = Math.min(
+        maxButtonWidth,
+        minButtonWidth + inputTextLength * 15
+    );
+
+    document.getElementById("add-button").style.width = currentButtonWidth + "px";
+}
+function resizeInput() {
+    const inputBox = document.getElementById("input-box");
+    inputBox.style.height = "auto"; // Reset height to auto
+    inputBox.style.height = inputBox.scrollHeight + "px"; // Set the height to match the content
+}
+
+// Call the resizeInput function initially to set the correct height for existing content
+resizeInput();
 
 const sr = ScrollReveal({
     origin: 'top',
